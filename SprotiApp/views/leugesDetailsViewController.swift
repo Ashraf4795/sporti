e90d85a54c8453454a370e709ba57eeeb8e60729
Ashraf4728 <codingcity4747@gmail.com>
2020-04-18 13:51:31 -0700

    design storyboard flow for sport,favourit and leuges,leagues details

---
+++ SprotiApp/views/leugesDetailsViewController.swift
@@ -0,0 +1,30 @@
+//
+//  leugesDetailsViewController.swift
+//  SprotiApp
+//
+//  Created by Ashraf on 4/18/20.
+//  Copyright © 2020 saraMohamed. All rights reserved.
+//
+
+import UIKit
+
+class leugesDetailsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
