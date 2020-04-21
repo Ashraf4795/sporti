672676ea56ebd9c4a3d30f863df3c304b769ef10
saraMohamed <saramohamed@192.168.233.133>
2020-04-20 19:44:37 -0700

    add coreData andFavourite leuges

---
+++ SprotiApp/model/LeagueC.swift
@@ -0,0 +1,16 @@
+//
+//  LeagueC.swift
+//  SprotiApp
+//
+//  Created by saraMohamed on 4/20/20.
+//  Copyright © 2020 saraMohamed. All rights reserved.
+//
+
+import Foundation
+class LeagueC :NSObject{
+    var leagueId:Int = 0
+    var strLeague:String = ""
+    var strCountry:String = ""
+    var strBadge:String = ""
+    var strYoutube:String = ""
+}
